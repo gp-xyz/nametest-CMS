834f365b1325208750215994d055a3f52883b29d
g <gabe.patgorski@gmail.com>
2023-03-06 19:58:09 -0800

    nothing

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -44,36 +44,39 @@ export default function Nav({ navSignedIn, assignTheme }) {
       })
   }
   return (
-   
-      <div className="grid grid-cols-1 md:grid-cols-5 my-3 w-full">
-        <div className="header-nav w-full col-span-4">
+
+    <div className="grid grid-cols-1 md:grid-cols-5 my-3 w-full">
+      <div className="header-nav w-full col-span-4">
 
 
 
-          <ul className="menu">
+        <ul className="menu">
 
-            <li className="bg-blue-300"><Link className="nav-link" to="/">Home</Link></li>
-            <li className="bg-yellow-300"><Link className="nav-link" to="/posts">Chronicle</Link></li>
-            <li className="bg-red-500"><Link className="nav-link" to="/monikers">Monikers</Link></li>
-            <li className="bg-purple-300"><Link className="nav-link" to="/about">about</Link></li>
-
-
-          </ul>
-
-          
+          <li className="bg-blue-300"><Link className="nav-link" to="/">Home</Link></li>
+          <li className="bg-yellow-300"><Link className="nav-link" to="/posts">Chronicle</Link></li>
+          <li className="bg-red-500"><Link className="nav-link" to="/monikers">Monikers</Link></li>
+          <li className="bg-purple-300"><Link className="nav-link" to="/about">about</Link></li>
 
 
-        </div>
-        <div className="logo relative hidden md:inline-flex">
-           <img alt="bottomlogo" src={logo} /> 
-           
-           </div>
-
+        </ul>
 
 
 
 
       </div>
+      <div className="logo relative hidden md:inline-flex h-full">
+        <div className="flex items-center w-52 bg-slate-700 rounded-md h-full">
+          <div className="text-white font-bold text-6xl md:text-8xl mx-3">F</div>
+          <div className="bg-red-500 w-20 h-20 rounded-full"> </div>
+        </div>
+
+      </div>
+
+
+
+
+
+    </div>
 
 
 
