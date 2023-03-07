862904b46493270961fb79430d548e93d7be6f65
g <gabe.patgorski@gmail.com>
2023-03-07 12:50:37 -0800

    tuesday 2

--- src/Components/CMS.jsx
+++ src/Components/CMS.jsx
@@ -5,53 +5,59 @@ function CMS() {
   const [posts, setPosts] = useState([]);
   const [authors, setAuthors] = useState({});
   const [media , setMedia] = useState({});
-  
+  const [isLoading, setIsLoading] = useState(true); // new state to track loading status
 
   useEffect(() => {
-    fetch("https://johpat90.dreamhosters.com/wp-json/wp/v2/posts?_embed")
-      .then(response => response.json())
-      .then(data => {
-        setPosts(data);
-      });
-    fetch("https://johpat90.dreamhosters.com/wp-json/wp/v2/media")
-    .then(response => response.json())
-    .then(data => {
-      const dataObject = data.reduce((acc, { id, source_url }) => {
+    Promise.all([
+      fetch("https://johpat90.dreamhosters.com/wp-json/wp/v2/posts?_embed"),
+      fetch("https://johpat90.dreamhosters.com/wp-json/wp/v2/media"),
+      fetch("https://johpat90.dreamhosters.com/wp-json/wp/v2/users")
+    ])
+    .then(([postResponse, mediaResponse, authorResponse]) => {
+      return Promise.all([
+        postResponse.json(),
+        mediaResponse.json(),
+        authorResponse.json()
+      ]);
+    })
+    .then(([postData, mediaData, authorData]) => {
+      const mediaObject = mediaData.reduce((acc, { id, source_url }) => {
         acc[id] = source_url;
         return acc;
       }, {});
+      setMedia(mediaObject);
 
-      setMedia(dataObject);
+      const authorObject = authorData.reduce((acc, author) => {
+        acc[author.id] = author;
+        return acc;
+      }, {});
+      setAuthors(authorObject);
 
-    });
-    fetch("https://johpat90.dreamhosters.com/wp-json/wp/v2/users")
-      .then(response => response.json())
-      .then(data => {
-     
-        const authorData = {};
-        data.forEach(author => {
-          authorData[author.id] = author;
-        });
-        
-       
-        setAuthors(authorData);
+      const postWithAuthor = postData.map(post => {
+        return {
+          ...post,
+          authorbox: authorObject[post.author],
+          avatar_url: mediaObject[authorObject[post.author]['acf']['avatar']],
+          img_url: mediaObject[post['acf']['focuspiece']]
+        };
       });
+      setPosts(postWithAuthor);
+      setIsLoading(false); // set loading to false after data is fetched
+    })
+    .catch(error => {
+      console.log(error);
+    });
   }, []);
 
-  const postsWithAuthors = posts.map(post => {
-    return {
-      ...post,
-      authorbox: authors[post.author],
-      avatar_url: media[authors[post.author]['acf']['avatar']],
-      img_url: media[post['acf']['focuspiece']]
-    };
-  });
+  if (isLoading) { // check if data is still loading
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className='general-light'>
       <div className="text-xl">The Latest from Flashbulb</div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
-        {postsWithAuthors.map(post => (
+        {posts.map(post => (
           <ChronPost post={post} />
         ))}
       </div>
