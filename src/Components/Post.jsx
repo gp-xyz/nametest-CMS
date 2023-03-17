d5007457346b7f557b923270685956cc9a5e4920
g <gabe.patgorski@gmail.com>
2023-03-16 19:47:04 -0700

    removedtheForce

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import ReactHtmlParser from "react-html-parser";
 
 function SinglePost() {
   const [post, setPost] = useState(null);
