ef2ca0a27afb5f2428e6a30a389a4c72e3fc259a
g <gabe.patgorski@gmail.com>
2023-03-21 16:31:55 -0700

    experimentHTTP

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -5,7 +5,7 @@ function SinglePost() {
   const [post, setPost] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const { slug } = useParams();
-  const wpurl = 'https://44.237.162.198/'
+  const wpurl = 'http://44.237.162.198/'
   const oldwpurl = 'https://johpat90.dreamhosters.com/'
   useEffect(() => {
     fetch(wpurl + `wp-json/wp/v2/posts?slug=${slug}&_embed`)
@@ -21,7 +21,7 @@ function SinglePost() {
   }, [slug]);
 
   if (isLoading || !post?.title?.rendered) {
-    return <div>Loading...</div>;
+    return <div>1 Loading...</div>;
   }
 
   return (
